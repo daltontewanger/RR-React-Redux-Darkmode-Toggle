58917c259efa5167103a8da2903916dcd380b48e
Dalton Tewanger <daltontewanger@yahoo.com>
2024-04-18 20:01:03 -0700

    Practiced dark and light theme toggles with Redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import './App.css';
-import Nav from './components/Nav'
-import ContentWrapper from './components/ContentWrapper'
-import Footer from './components/Footer'
+import "./App.css";
+import Nav from "./components/Nav";
+import ContentWrapper from "./components/ContentWrapper";
+import Footer from "./components/Footer";
+import { useSelector, useDispatch } from "react-redux";
+import { setDarkTheme, setLightTheme } from "./features/themeSlice";
 
 function App() {
-  
+  const dispatch = useDispatch();
+  const theme = useSelector((state) => state.theme);
+
+  const handleOnClick = () => {
+    if (theme.darkMode) {
+      dispatch(setLightTheme());
+    } else {
+      dispatch(setDarkTheme());
+    }
+  };
   return (
-    <div style={{ backgroundColor: 'white', color: 'black' }} className="App">
+    <div
+      style={{ backgroundColor: theme.color1, color: theme.color5 }}
+      className="App"
+    >
+      <button onClick={handleOnClick}>Set Theme</button>
       <Nav />
       <ContentWrapper />
       <Footer />
